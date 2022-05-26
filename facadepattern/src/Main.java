6c6de5743168f34d9847d13210bba739f9ede5e5
LasindaDilshan <32431966+LasindaDilshan@users.noreply.github.com>
2022-05-26 23:37:20 +0530

    Add files via upload

---
+++ facadepattern/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("***Facade Pattern Demo***\n");
+        //Creating Robots
+        RobotFacade milanoRobotFacade = new RobotFacade();
+        milanoRobotFacade.constructMilanoRobot();
+        RobotFacade robonautRobotFacade = new RobotFacade();
+        robonautRobotFacade.constructRobonautRobot();
+        //Destroying robots
+        milanoRobotFacade.destroyMilanoRobot();
+        robonautRobotFacade.destroyRobonautRobot()
+
+    }
+}
